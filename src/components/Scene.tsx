d8964c79ed51817a1df04e7c849c1ef7b1b89684
minjae0402 <kangjack0402@gmail.com>
2024-12-23 10:18:27 +0900

    Add : Tree

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,4 +1,4 @@
-import {  Html, OrbitControls, Stats, useProgress } from '@react-three/drei';
+import {  Html, OrbitControls,useProgress } from '@react-three/drei';
 import { Canvas, useLoader } from '@react-three/fiber';
 import React, { Suspense } from 'react';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
@@ -11,7 +11,7 @@ function Loader() {
 }
 
 const Scene: React.FC = () => {
-  const gltf = useLoader(GLTFLoader, '/models/untitled.glb')
+  const gltf = useLoader(GLTFLoader, '/models/tree.glb')
 
   return (
     <Suspense fallback={<Loader />}>
@@ -28,8 +28,7 @@ const Scene: React.FC = () => {
         />
         
         <OrbitControls target={[0, 1, 0]} />
-        <axesHelper args={[5]} />
-        <Stats />
+        
       </Canvas>
     </Suspense>
   );
