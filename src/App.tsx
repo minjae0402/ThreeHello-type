175ef72d1a728aebb08d4698cc82c56cb8c561a6
minjae0402 <kangjack0402@gmail.com>
2024-12-23 09:58:53 +0900

    init : glb file loder

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Scene from './components/Scene';
+
+const App: React.FC = () => {
+
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Scene />
+    </div>
+  );
+};
+
+export default App;
